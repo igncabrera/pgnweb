26dc9cc63d74fd960de6f8d23a879de8362616bd
igncabrera <88503093+igncabrera@users.noreply.github.com>
2021-08-22 18:15:32 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,28 +1,14 @@
 var intento = 3; //Numero de intentos disponibles
 
 function validar() {
-    var username = document.getElementById("username").value;
 
     var password = document.getElementById("password").value;
 
-    if (password.length >= 8) {
-        if (username == "0" && password == "12345678910") {
+    if (password.length >= 8) {     
             alert("Accediste exitosamente");
             window.location = "mainmenu.html"; // Redirecciona a la otra pagina
             intento = 3
-            return false;
-        }
-        else {
-            intento--;// Resta intentos de la variable inicial si son incorrectos los datos
-            alert("Te quedan " + intento + " intentos;");
-            // Deshabilita la opcion de poner mas datos si se queda sin intentos
-            if (intento == 0) {
-                document.getElementById("username").disabled = true;
-                document.getElementById("password").disabled = true;
-                document.getElementById("submit").disabled = true;
-                return false;
-            }
-        }
+            return false; 
     } else {
         alert("La contraseña debe tener mas de 8 caracteres")
     }
