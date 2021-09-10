d4e7b5a400a235bdada8f4d014171d60f6a0b0cb
igncabrera <88503093+igncabrera@users.noreply.github.com>
2021-09-10 10:15:31 -0300

    responsive

--- js/usuario.js
+++ js/usuario.js
@@ -1,4 +1,5 @@
-var texto = `<a class="py-2 d-none d-md-inline-block" href="my-profile.html">` + sessionStorage.getItem("username") + `</a>`
+var texto = `<a class="nav-link" href="my-profile.html">`+ sessionStorage.getItem("username") +`</a>`
+
 if (sessionStorage.getItem("username") !== null) {
   document.getElementById("logger").innerHTML += texto
 }
